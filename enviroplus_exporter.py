f80aa65d5f7e5e2ec2c02eb0ee20370adf2b8935
Amadeusz Kryze <amadeusz.kryze@gmail.com>
2023-05-06 19:55:36 +0200

    Herping the derp

--- enviroplus_exporter.py
+++ enviroplus_exporter.py
@@ -59,6 +59,8 @@ Press Ctrl+C to exit!
 
 DEBUG = os.getenv('DEBUG', 'false') == 'true'
 
+RBPI_SERIAL = ""
+
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 noise = Noise()
@@ -91,17 +93,6 @@ HEIGHT = st7735.height
 # Set up canvas and font
 img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
 draw = ImageDraw.Draw(img)
-font_size_small = 10
-font_size_large = 20
-font = ImageFont.truetype(UserFont, font_size_large)
-smallfont = ImageFont.truetype(UserFont, font_size_small)
-x_offset = 2
-y_offset = 2
-
-message = ""
-
-# The position of the top bar
-top_pos = 25
 
 # Define your own warning limits
 # The limits definition follows the order of the variables array
@@ -148,36 +139,36 @@ except ValueError:
 
 PREFIX = os.getenv('PREFIX', 'enviroplus_')
 
-TEMPERATURE = Gauge(PREFIX+'temperature','Temperature measured (*C)')
-PRESSURE = Gauge(PREFIX+'pressure','Pressure measured (hPa)')
-HUMIDITY = Gauge(PREFIX+'humidity','Relative humidity measured (%)')
-OXIDISING = Gauge(PREFIX+'oxidising','Mostly nitrogen dioxide but could include NO and Hydrogen (Ohms)')
-REDUCING = Gauge(PREFIX+'reducing', 'Mostly carbon monoxide but could include H2S, Ammonia, Ethanol, Hydrogen, Methane, Propane, Iso-butane (Ohms)')
-NH3 = Gauge(PREFIX+'NH3', 'mostly Ammonia but could also include Hydrogen, Ethanol, Propane, Iso-butane (Ohms)')
-LUX = Gauge(PREFIX+'lux', 'current ambient light level (lux)')
-PROXIMITY = Gauge(PREFIX+'proximity', 'proximity, with larger numbers being closer proximity and vice versa')
-PM1 = Gauge(PREFIX+'PM1', 'Particulate Matter of diameter less than 1 micron. Measured in micrograms per cubic metre (ug/m3)')
-PM25 = Gauge(PREFIX+'PM25', 'Particulate Matter of diameter less than 2.5 microns. Measured in micrograms per cubic metre (ug/m3)')
-PM10 = Gauge(PREFIX+'PM10', 'Particulate Matter of diameter less than 10 microns. Measured in micrograms per cubic metre (ug/m3)')
-CPU_TEMPERATURE = Gauge(PREFIX+'cpu_temperature','CPU temperature measured (*C)')
-BATTERY_VOLTAGE = Gauge(PREFIX+'battery_voltage','Voltage of the battery (Volts)')
-BATTERY_PERCENTAGE = Gauge(PREFIX+'battery_percentage','Percentage of the battery remaining (%)')
+TEMPERATURE = Gauge(PREFIX+'temperature','Temperature measured (*C)', ['serial'], ['serial'])
+PRESSURE = Gauge(PREFIX+'pressure','Pressure measured (hPa)', ['serial'])
+HUMIDITY = Gauge(PREFIX+'humidity','Relative humidity measured (%)', ['serial'])
+OXIDISING = Gauge(PREFIX+'oxidising','Mostly nitrogen dioxide but could include NO and Hydrogen (Ohms)', ['serial'])
+REDUCING = Gauge(PREFIX+'reducing', 'Mostly carbon monoxide but could include H2S, Ammonia, Ethanol, Hydrogen, Methane, Propane, Iso-butane (Ohms)', ['serial'])
+NH3 = Gauge(PREFIX+'NH3', 'mostly Ammonia but could also include Hydrogen, Ethanol, Propane, Iso-butane (Ohms)', ['serial'])
+LUX = Gauge(PREFIX+'lux', 'current ambient light level (lux)', ['serial'])
+PROXIMITY = Gauge(PREFIX+'proximity', 'proximity, with larger numbers being closer proximity and vice versa', ['serial'])
+PM1 = Gauge(PREFIX+'PM1', 'Particulate Matter of diameter less than 1 micron. Measured in micrograms per cubic metre (ug/m3)', ['serial'])
+PM25 = Gauge(PREFIX+'PM25', 'Particulate Matter of diameter less than 2.5 microns. Measured in micrograms per cubic metre (ug/m3)', ['serial'])
+PM10 = Gauge(PREFIX+'PM10', 'Particulate Matter of diameter less than 10 microns. Measured in micrograms per cubic metre (ug/m3)', ['serial'])
+CPU_TEMPERATURE = Gauge(PREFIX+'cpu_temperature','CPU temperature measured (*C)', ['serial'])
+BATTERY_VOLTAGE = Gauge(PREFIX+'battery_voltage','Voltage of the battery (Volts)', ['serial'])
+BATTERY_PERCENTAGE = Gauge(PREFIX+'battery_percentage','Percentage of the battery remaining (%)', ['serial'])
 
-OXIDISING_HIST = Histogram(PREFIX+'oxidising_measurements', 'Histogram of oxidising measurements', buckets=(0, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 100000))
-REDUCING_HIST = Histogram(PREFIX+'reducing_measurements', 'Histogram of reducing measurements', buckets=(0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000))
-NH3_HIST = Histogram(PREFIX+'nh3_measurements', 'Histogram of nh3 measurements', buckets=(0, 10000, 110000, 210000, 310000, 410000, 510000, 610000, 710000, 810000, 910000, 1010000, 1110000, 1210000, 1310000, 1410000, 1510000, 1610000, 1710000, 1810000, 1910000, 2000000))
+OXIDISING_HIST = Histogram(PREFIX+'oxidising_measurements', 'Histogram of oxidising measurements', ['serial'], buckets=(0, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 100000))
+REDUCING_HIST = Histogram(PREFIX+'reducing_measurements', 'Histogram of reducing measurements', ['serial'], buckets=(0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000))
+NH3_HIST = Histogram(PREFIX+'nh3_measurements', 'Histogram of nh3 measurements', ['serial'], buckets=(0, 10000, 110000, 210000, 310000, 410000, 510000, 610000, 710000, 810000, 910000, 1010000, 1110000, 1210000, 1310000, 1410000, 1510000, 1610000, 1710000, 1810000, 1910000, 2000000))
 
-PM1_HIST = Histogram(PREFIX+'pm1_measurements', 'Histogram of Particulate Matter of diameter less than 1 micron measurements', buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
-PM25_HIST = Histogram(PREFIX+'pm25_measurements', 'Histogram of Particulate Matter of diameter less than 2.5 micron measurements', buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
-PM10_HIST = Histogram(PREFIX+'pm10_measurements', 'Histogram of Particulate Matter of diameter less than 10 micron measurements', buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
+PM1_HIST = Histogram(PREFIX+'pm1_measurements', 'Histogram of Particulate Matter of diameter less than 1 micron measurements', ['serial'], buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
+PM25_HIST = Histogram(PREFIX+'pm25_measurements', 'Histogram of Particulate Matter of diameter less than 2.5 micron measurements', ['serial'], buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
+PM10_HIST = Histogram(PREFIX+'pm10_measurements', 'Histogram of Particulate Matter of diameter less than 10 micron measurements', ['serial'], buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
 
-NOISE_PROFILE_LOW_FREQ = Gauge(PREFIX+'noise_profile_low_freq', 'Noise profile of low frequency noise (db)')
-NOISE_PROFILE_MID_FREQ = Gauge(PREFIX+'noise_profile_mid_freq', 'Noise profile of mid frequency noise (db)')
-NOISE_PROFILE_HIGH_FREQ = Gauge(PREFIX+'noise_profile_high_freq', 'Noise profile of high frequency noise (db)')
-NOISE_PROFILE_AMP = Gauge(PREFIX+'noise_profile_amp', 'Noise profile of amplitude (db)')
+NOISE_PROFILE_LOW_FREQ = Gauge(PREFIX+'noise_profile_low_freq', 'Noise profile of low frequency noise (db)', ['serial'])
+NOISE_PROFILE_MID_FREQ = Gauge(PREFIX+'noise_profile_mid_freq', 'Noise profile of mid frequency noise (db)', ['serial'])
+NOISE_PROFILE_HIGH_FREQ = Gauge(PREFIX+'noise_profile_high_freq', 'Noise profile of high frequency noise (db)', ['serial'])
+NOISE_PROFILE_AMP = Gauge(PREFIX+'noise_profile_amp', 'Noise profile of amplitude (db)', ['serial'])
 
 # delay between each write to lcd
-WRITE_TO_LCD_TIME = int(os.getenv('WRITE_TO_LCD_TIME', '4'))
+WRITE_TO_LCD_TIME = int(os.getenv('WRITE_TO_LCD_TIME', '2'))
 
 # Sometimes the sensors can't be read. Resetting the i2c
 def reset_i2c():
@@ -205,7 +196,7 @@ def get_cpu_temperature():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
         temp = f.read()
         temp = int(temp) / 1000.0
-        CPU_TEMPERATURE.set(temp)
+        CPU_TEMPERATURE.labels(RBPI_SERIAL).set(temp)
 
 def get_temperature(factor_usr):
     """Get temperature from the weather sensor"""
@@ -235,13 +226,13 @@ def get_temperature(factor_usr):
     # avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
     # temperature = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
 
-    TEMPERATURE.set(temperature)   # Set to a given value
+    TEMPERATURE.labels(RBPI_SERIAL).set(temperature)   # Set to a given value
 
 def get_pressure():
     """Get pressure from the weather sensor"""
     try:
         pressure = bme280.get_pressure()
-        PRESSURE.set(pressure)
+        PRESSURE.labels(RBPI_SERIAL).set(pressure)
     except IOError:
         logging.error("Could not get pressure readings. Resetting i2c.")
         reset_i2c()
@@ -250,7 +241,7 @@ def get_humidity(humidity_compensation):
     """Get humidity from the weather sensor"""
     try:
         humidity = bme280.get_humidity()
-        HUMIDITY.set(humidity)
+        HUMIDITY.labels(RBPI_SERIAL).set(humidity)
     except IOError:
         logging.error("Could not get humidity readings. Resetting i2c.")
         reset_i2c()
@@ -259,13 +250,13 @@ def get_gas():
     """Get all gas readings"""
     try:
         readings = gas.read_all()
-        OXIDISING.set(readings.oxidising)
+        OXIDISING.labels(RBPI_SERIAL).set(readings.oxidising)
         OXIDISING_HIST.observe(readings.oxidising)
 
-        REDUCING.set(readings.reducing)
+        REDUCING.labels(RBPI_SERIAL).set(readings.reducing)
         REDUCING_HIST.observe(readings.reducing)
 
-        NH3.set(readings.nh3)
+        NH3.labels(RBPI_SERIAL).set(readings.nh3)
         NH3_HIST.observe(readings.nh3)
     except IOError:
         logging.error("Could not get gas readings. Resetting i2c.")
@@ -278,10 +269,10 @@ def get_noise_profile():
         mid = noise.get_amplitude_at_frequency_range(200, 2000)
         high = noise.get_amplitude_at_frequency_range(2000, 8000)
         amp = noise.get_amplitude_at_frequency_range(20, 8000)
-        NOISE_PROFILE_LOW_FREQ.set(low)
-        NOISE_PROFILE_MID_FREQ.set(mid)
-        NOISE_PROFILE_HIGH_FREQ.set(high)
-        NOISE_PROFILE_AMP.set(amp)
+        NOISE_PROFILE_LOW_FREQ.labels(RBPI_SERIAL).set(low)
+        NOISE_PROFILE_MID_FREQ.labels(RBPI_SERIAL).set(mid)
+        NOISE_PROFILE_HIGH_FREQ.labels(RBPI_SERIAL).set(high)
+        NOISE_PROFILE_AMP.labels(RBPI_SERIAL).set(amp)
     except IOError:
         logging.error("Could not get noise profile. Resetting i2c.")
         reset_i2c()
@@ -292,8 +283,8 @@ def get_light():
        lux = ltr559.get_lux()
        prox = ltr559.get_proximity()
 
-       LUX.set(lux)
-       PROXIMITY.set(prox)
+       LUX.labels(RBPI_SERIAL).set(lux)
+       PROXIMITY.labels(RBPI_SERIAL).set(prox)
     except IOError:
         logging.error("Could not get lux and proximity readings. Resetting i2c.")
         reset_i2c()
@@ -307,9 +298,9 @@ def get_particulates():
     except (IOError, pmsSerialTimeoutError):
         logging.warning("Could not get particulate matter readings.")
     else:
-        PM1.set(pms_data.pm_ug_per_m3(1.0))
-        PM25.set(pms_data.pm_ug_per_m3(2.5))
-        PM10.set(pms_data.pm_ug_per_m3(10))
+        PM1.labels(RBPI_SERIAL).set(pms_data.pm_ug_per_m3(1.0))
+        PM25.labels(RBPI_SERIAL).set(pms_data.pm_ug_per_m3(2.5))
+        PM10.labels(RBPI_SERIAL).set(pms_data.pm_ug_per_m3(10))
 
         PM1_HIST.observe(pms_data.pm_ug_per_m3(1.0))
         PM25_HIST.observe(pms_data.pm_ug_per_m3(2.5) - pms_data.pm_ug_per_m3(1.0))
@@ -320,8 +311,8 @@ def get_battery():
     try:
         voltage_reading = sensor.cell_voltage
         percentage_reading = sensor.cell_percent
-        BATTERY_VOLTAGE.set(voltage_reading)
-        BATTERY_PERCENTAGE.set(percentage_reading)
+        BATTERY_VOLTAGE.labels(RBPI_SERIAL).set(voltage_reading)
+        BATTERY_PERCENTAGE.labels(RBPI_SERIAL).set(percentage_reading)
         if DEBUG:
             logging.info("Battery: {} Volts / {} %".format(sensor.cell_voltage, sensor.cell_percent))
     except (RuntimeError, OSError) as exception:
@@ -353,21 +344,26 @@ def collect_all_data():
 
 def write_to_lcd():
     """Write dta to eniro lcd"""
+    time.sleep(WRITE_TO_LCD_TIME)
+
     while True:
-        time.sleep(WRITE_TO_LCD_TIME)
         sensor_data = collect_all_data()
 
         try:
             variables = [
                 "temperature",
+                "cpu_temperature",
                 "pressure",
                 "humidity",
+                "lux",
             ]
 
             units = [
-                    "°C",
-                    "hPa",
-                    "%",
+                "°C",
+                "°C",
+                "hPa",
+                "%",
+                "lx",
             ]
 
             for i in range(len(variables)):
@@ -421,6 +417,8 @@ if __name__ == '__main__':
     parser.add_argument("-P", "--polling", metavar='POLLING', type=int, default=2, help="Polling interval in seconds, to fetch data from sensor [default: 2]")
     parser.add_argument("-L", "--lcd", metavar='LCD', type=str_to_bool, default='false', help="Display sensor data on LCD [default: false]")
     args = parser.parse_args()
+
+    RBPI_SERIAL = get_serial_number()
 
     # Start up the server to expose the metrics.
     start_http_server(addr=args.bind, port=args.port)
